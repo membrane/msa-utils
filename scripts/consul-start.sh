7c299c3b0b5887f30bbfbe9eb76ec1267d6bd60b
oweiler <weiler@predic8.de>
2018-03-15 16:06:17 +0100

    consul-start.sh will download consul if not already present

--- scripts/consul-start.sh
+++ scripts/consul-start.sh
@@ -1,1 +1,7 @@
+# will dowload consul if necessary
+
+if [[ ! -f consul ]]; then
+  wget https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip && unzip consul*.zip
+fi
+
 ./consul agent -data-dir=/tmp/consul -bind 127.0.0.1 -ui -dev
