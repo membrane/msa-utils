b9617bdf2f74f6854444f55dbe8148385c50aebf
thomas <bayer@predic8.de>
2018-03-15 09:47:07 +0100

    Consul start script added

--- scripts/consul-start.sh
+++ scripts/consul-start.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/env bash
-
-docker rm -f consul
-docker run -d --name=consul -e CONSUL_BIND_INTERFACE=eth0 -p 8500:8500 consul
+./consul agent -data-dir=/tmp/consul -bind 127.0.0.1 -ui -dev
