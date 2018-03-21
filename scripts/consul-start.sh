8da8f1d2c36b81c34f70cd806a6ef47bbcc46bea
Daniel Bonnauer <bonnauer@predic8.de>
2018-03-21 11:53:12 +0100

    added support for consul on mac

--- scripts/consul-start.sh
+++ scripts/consul-start.sh
@@ -1,7 +1,14 @@
 # will dowload consul if necessary
 
 if [[ ! -f consul ]]; then
-  wget https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip && unzip consul*.zip
+	if [[ $OSTYPE == "darwin"* ]]; then
+		wget https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_darwin_amd64.zip
+	fi
+	if [[ $OSTYPE == "linux-gnu" ]]; then
+		wget https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip
+	fi  
 fi
+
+unzip consul*.zip
 
 ./consul agent -data-dir=/tmp/consul -bind 127.0.0.1 -ui -dev
