f4af9b8338c23bab79f01b414c6b2ee03cdcd8a7
thomas <bayer@predic8.de>
2018-09-20 12:15:17 +0200

    fixed linebreak

--- scripts/kafka-start.sh
+++ scripts/kafka-start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 docker rm -f kafka
 docker run -d --name kafka -e ADVERTISED_HOST=localhost -p 2181:2181 -p 9092:9092 --env ADVERTISED_PORT=9092 -h kafka spotify/kafka
